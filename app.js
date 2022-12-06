5aaa32904167c958161f611909c7366eb0eb21ae
yashnerkar <yashnerkar512@gmail.com>
2022-12-06 16:11:43 +0530

    update app.js in server

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use("/", contacts);
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
